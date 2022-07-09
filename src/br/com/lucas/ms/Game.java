34971dc58dc66cc3f7b1d40a32f5f130dc16c119
Lucas Pina <lucaspinarj@hotmail.com>
2022-07-09 01:07:01 -0300

    Skeleton of the project
    Added some packages, classes and JUnit5 library.

---
+++ src/br/com/lucas/ms/Game.java
@@ -0,0 +1,5 @@
+package br.com.lucas.ms;
+
+public class Game {
+
+}
---
+++ test/br/com/lucas/ms/TestWindow.java
@@ -0,0 +1,14 @@
+package br.com.lucas.ms;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestWindow {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
